4b5467381077bb42f0b6575e2fb95d2b97cac79e
Arnau López <108700238+alopezpag@users.noreply.github.com>
2023-11-25 00:21:33 +0100

    created but no finished enum VisaType

--- src/main/java/edu/uoc/pac3/VisaType.java
+++ src/main/java/edu/uoc/pac3/VisaType.java
@@ -1,5 +1,47 @@
 package edu.uoc.pac3;
 
 public enum VisaType {
+    TOURIST("Tourist Visa", "T", 123),
+    BUSINESS("Business Visa", "B", 456),
+    STUDENT("Student Visa", "S", 789),
+    WORK("Work Visa", "W", 321),
+    TRANSIT("Transit Visa", "TR", 654),
+    FAMILY("Family Visa", "F", 987),
+    JOURNALIST("Journalist Visa", "J", 234),
+    MEDICAL("Medical Visa", "M", 567),
+    RETIREMENT("Retirement Visa", "R", 890),
+    INVESTOR("Investor Visa", "I", 432),
+    DIPLOMATIC("Diplomatic Visa", "D", 765),
+    SCHENGEN("Schengen Visa", "SCH", 198),
+    EMPLOYMENT("Employment Visa", "E", 543),
+    VISITING_FRIENDS("Visiting Friends Visa", "VF", 876),
+    RELIGIOUS("Religious Visa", "RL", 321),
+    OTHER("Other Visa", "O", 654);
 
+    private final String description;
+    private final String stringIdentifier;
+    private final int intIdentifier;
+
+    VisaType(String description, String stringIdentifier, int intIdentifier) {
+        this.description = description;
+        this.stringIdentifier = stringIdentifier;
+        this.intIdentifier = intIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStringIdentifier() {
+        return stringIdentifier;
+    }
+
+    public int getIntIdentifier() {
+        return intIdentifier;
+    }
+
+    @Override
+    public String toString() {
+        return this.name() + " | " + this.description + " | '" + this.stringIdentifier + "' | " + this.intIdentifier;
+    }
 }
